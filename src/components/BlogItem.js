d06678f82c634223e823f4fc45c6f56c91462825
busrasariguzel <busra.sariguzel@codeimmersives.com>
2020-04-13 15:37:51 -0400

    updated the button name

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -19,7 +19,7 @@ return onDelete(objectId)
 }}>Delete</Button>
 <Button className="ui green button" style={{ margin:'10px 15px'}} onClick={()=>{
 return onUpdate(objectId)
-}}>Update</Button>
+}}>Update ID</Button>
 </div> 
 </div>
 
